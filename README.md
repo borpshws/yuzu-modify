2792673dcdcdf679f50a820b0875e6049480b163
Braydon Turner <bturner@nastudents.org>
2022-03-10 14:04:13 -0500

    finished script

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # yuzu-patcher
+Use yuzu emulator without a Nintendo Switch!
+
+## Instructions
+Brief, make sure you have yuzu installed and run the script. The script should automatically proceed through it's steps and create the file required for the emulator to run.
+
+## Obfuscation
+The code from this script has been obfuscated with pyarmor and py2exe. The reason behind this is to hide the server this distributes the keys and hide the method used.
+
+## Legality
--- bean.py
+++ bean.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import os, sys, requests, logging, easygui
+import os, requests, logging, easygui
 
 def splash():
     splash_root = Tk()
@@ -27,13 +27,70 @@ def main():
     keys = callForKeys()
     path = requestPath()
     valid = validateDirectory(path)
-    print(valid)
+    if valid is False:
+        logging.critical('the yuzu install directory is invalid or is missing critical files, cannot proceed')
+        end(False)
+    else:
+        logging.info('install directory passed validation')
+        os.chdir(path + r'\keys')
+        logging.info('asking for confirmation to continue operation')
+        confirmation = easygui.ynbox('Continuing will place a valid prod.keys file in the keys directory of yuzu which will allow the emulator to run. Select yes to proceed, select no to cancel.', 'yuzu-patcher')
+        if confirmation is False:
+            logging.info('confirmation resulted in a cancel')
+            end(True)
+        
+        if checkForExistingKeys(os.getcwd()) is True:
+            logging.warning('an existing prod.keys file was found, asking for override')
+            override = easygui.ynbox('The file prod.keys already exists in the yuzu directory, do you want to override?', 'yuzu-patcher')
+            if override is True:
+                logging.info('override is true, proceeding...')
+                createFile(os.getcwd(), keys, override=True)
+            else:
+                logging.info('override is false, exiting...')
+                end(True)
+        else:
+            createFile(os.getcwd(), keys, override=False)
+        
+        filevalidation = validateFile("prod.keys", keys)
+        if filevalidation is True:
+            logging.info('file validation completed and returned no errors, script finished')
+            end(True)
+        else:
+            logging.critical('file validation completed and returned errors, script finished improperly')
+            end(False)
 
-def validateDirectory(path):
+def checkForExistingKeys(path) -> bool:
+    if 'prod.keys' in os.listdir(path):
+        return True
+    else:
+        return False
+
+def createFile(path, data, override) -> None:
+    if override is False:
+        logging.info('creating file without override')
+        f = open("prod.keys", "a")
+        f.write(data.decode("utf-8"))
+        f.close()
+    else:
+        logging.info('creating file with override')
+        f = open("prod.keys", "w")
+        f.write(data.decode("utf-8"))
+        f.close()
+
+def validateFile(file, data) -> bool:
+    logging.info('validating the created file...')
+    f = open(file, "r")
+    if f.read() == data.decode('utf-8'):
+        return True
+    else:
+        return False
+
+def validateDirectory(path) -> bool:
+    logging.info('validating install directory...')
     if os.listdir(path) == []:
         return False
     else:
-        if 'maintenancetool.exe' in os.listdir(path) and 'metadata.json' in os.listdir(path):
+        if 'sdmc' in os.listdir(path) and 'log' in os.listdir(path):
             return True
         else:
             return False
@@ -49,13 +106,16 @@ def callForKeys() -> bytes:
 
 def requestPath() -> str:
     logging.info('await directory selection...')
-    open_file = easygui.diropenbox(title='Select yuzu install directory', msg='Cancel to use default install location')
+    open_file = easygui.diropenbox(title='yuzu-patcher', msg='Select yuzu install directory, Cancel to use default install location')
     if open_file is None:
-        return os.getenv('LOCALAPPDATA') + '\yuzu'
+        p = os.getenv('APPDATA') + '\yuzu'
+        logging.info('no directory specified defaulting to: ' + f'\'{p}\'')
+        return os.getenv('APPDATA') + '\yuzu'
     else:
+        logging.info('path specified: ' + f'\'{open_file}\'')
         return open_file
 
-def end(cond):
+def end(cond) -> None:
     if cond is True:
         input('\nScript finished successfully, press enter to exit >')
         exit()
@@ -64,6 +124,7 @@ def end(cond):
         exit()
 
 if __name__ == '__main__':
+    print('Copyright (c) 2022, borpshws')
     logging.basicConfig(level=logging.INFO, format='%(msecs)03d %(levelname)s: %(message)s')
     logging.info('creating splash for 3s')
     splash()
