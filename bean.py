b8b3e77b19092379f62e031ec0396239a5201222
Braydon Turner <braydonturn@gmail.com>
2022-03-09 23:07:11 -0500

    finish splash, grab keys from server, check for install dir, created end conditions

--- bean.py
+++ bean.py
@@ -1,38 +1,69 @@
 from tkinter import *
-import os, sys, requests, logging
+import os, sys, requests, logging, easygui
 
-def main():
+def splash():
     splash_root = Tk()
     splash_root.overrideredirect(1)
-    splash_root.geometry("200x200")
-    splash_label = Label(splash_root, text='Splash Screen', font=18)
+    splash_root.geometry("512x512")
+    splash_root.lift()
+    splash_root.image = PhotoImage(file='splash.png')
+    splash_label = Label(splash_root, image=splash_root.image, bg='white')
+    splash_root.eval('tk::PlaceWindow . center')
+    splash_root.wm_attributes("-topmost", True)
+    splash_root.wm_attributes("-disabled", True)
+    splash_root.wm_attributes("-transparentcolor", "white")
     splash_label.pack()
 
-    def main2():
+    def splash2():
         splash_root.destroy()
-        fly()
+        logging.info('splash end')
+        main()
 
-    splash_root.after(3000, main2)
+    splash_root.after(3000, splash2)
     mainloop()
 
-def fly():
+def main():
     logging.info('main function entered, calling for keys...')
-    callForKeys()
+    keys = callForKeys()
+    path = requestPath()
+    valid = validateDirectory(path)
+    print(valid)
 
-def callForKeys() -> str:
-    res = requests.get('https://raw.githubusercontent.com/emuworld/aio/master/prod.key')
+def validateDirectory(path):
+    if os.listdir(path) == []:
+        return False
+    else:
+        if 'maintenancetool.exe' in os.listdir(path) and 'metadata.json' in os.listdir(path):
+            return True
+        else:
+            return False
+
+def callForKeys() -> bytes:
+    res = requests.get('https://raw.githubusercontent.com/emuworld/aio/master/prod.keys')
     if res.status_code != 200:
-        logging.critical('call to key server failed')
+        logging.critical('call to key server resulted in a status code ≠ 200, cannot proceed')
         end(False)
     else:
-        print(str(res.content))
+        logging.info('keys successfully retrived from server')
+        return res.content
+
+def requestPath() -> str:
+    logging.info('await directory selection...')
+    open_file = easygui.diropenbox(title='Select yuzu install directory', msg='Cancel to use default install location')
+    if open_file is None:
+        return os.getenv('LOCALAPPDATA') + '\yuzu'
+    else:
+        return open_file
 
 def end(cond):
     if cond is True:
         input('\nScript finished successfully, press enter to exit >')
+        exit()
     else:
         input('\nScript failed, press enter to exit >')
+        exit()
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    main()
+    logging.basicConfig(level=logging.INFO, format='%(msecs)03d %(levelname)s: %(message)s')
+    logging.info('creating splash for 3s')
+    splash()
